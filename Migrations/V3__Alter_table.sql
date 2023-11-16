48de7c9a1414dad291d617445756169a67256a7c
Kirill-Efremov <kirillefr45@gmail.com>
2023-11-16 23:20:19 +0300

    homework 5 Kirill

--- Migrations/V3__Alter_table.sql
+++ Migrations/V3__Alter_table.sql
@@ -1,2 +1,10 @@
 ALTER TABLE City
-ADD COLUMN rayons_count int CHECK ( rayons_count>0 and rayons_count<50 )
+ADD COLUMN rayons_count int CHECK ( rayons_count>0 and rayons_count<50 );
+ALTER TABLE Fan
+ADD COLUMN height integer check(height>140 and height<250),
+ADD COLUMN weight integer check(weight>40 and weight<200);
+
+ALTER TABLE Coach
+DROP COLUMN age,
+ADD COLUMN birth_date date NOT NULL CHECK(birth_date < current_date);
+
